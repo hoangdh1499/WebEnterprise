4376e7641edd365adb96f02a42c8b7b1f8622e80
hoangdh1499 <77620208+hoangdh1499@users.noreply.github.com>
2021-02-07 16:15:06 +0700

    check database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebEnterprise.Models;
 
 namespace WebEnterprise.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private G5EnterpriseDBEntities db = new G5EnterpriseDBEntities();
+        public ActionResult Index(string searchString)
         {
-            return View();
+            var ct = from m in db.CTents select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ct = ct.Where(s => s.CTname.Contains(searchString));
+            }
+            return View(ct);
+
         }
 
         public ActionResult About()
